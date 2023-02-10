e0fc963f04625cf3ed8c11777f8dd2c5bc7a35e4
zphj1987 <zphj1987@gmail.com>
2023-02-10 16:05:52 +0800

    修改编译脚本

--- lmt-3.2.11/build-nosql.sh
+++ lmt-3.2.11/build-nosql.sh
@@ -5,6 +5,6 @@ cp -ra lmt-3.2.11.tar.gz /root/rpmbuild/SOURCES/;
 rm -rf /root/rpmbuild/RPMS/x86_64/lmt*.rpm
 rpmbuild -bb lmt-nomysql.spec
 
-rpm -ivh /root/rpmbuild/RPMS/x86_64/lmt*.rpm --force
-killall cerebrod
-systemctl restart cerebrod
+#rpm -ivh /root/rpmbuild/RPMS/x86_64/lmt*.rpm --force
+#killall cerebrod
+#systemctl restart cerebrod
